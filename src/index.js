bfd8d76bb1592bf4c56a4d0ca060bb13913f75e5
pushpa446 <pdumpala@thoughtworks.com>
2017-04-25 15:10:32 +0530

    add mocha chai frameworks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = (props) => <h1>Hello world!</h1>;
+
 ReactDOM.render(
-    <h1>Hello world</h1>,
+    <App/>,
         document.getElementById('main'));
---
+++ test/app.test.js
@@ -0,0 +1,5 @@
+describe('App', function () {
+    it('should print 1 equals to 1', function () {
+        if(1 !== 1) throw  new Error("failed")
+    });
+});
