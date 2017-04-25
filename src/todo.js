522249dc23a7aaf807c21ad3ce47a7f2b3e04f26
pushpa446 <pdumpala@thoughtworks.com>
2017-04-25 18:04:07 +0530

    add button to todo item

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,6 @@
 import React from 'react';
 
-export const Todo = (props) => <div>{props.completed ? <del>props.text</del> : props.text}</div>;
+export const Todo = (props) => <div>
+    <button>✔</button>
+    <span>{props.completed ? <del>{props.text}</del> : props.text}</span>
+</div>;
--- test/todo.test.js
+++ test/todo.test.js
@@ -1,18 +1,26 @@
+import chai,{expect} from 'chai';
 import React from 'react';
 import {shallow} from 'enzyme';
-import {Todo} from '../src/todo';
-import chai,{expect} from 'chai';
 import chaiEnzyme from 'chai-enzyme';
+
+import {Todo} from '../src/todo';
 
 describe('Todo',() => {
    it('should render children', () => {
        const wrapper = shallow(<Todo completed={false} text="hello"/>);
        expect(wrapper).to.have.descendants('div');
-       expect(wrapper).to.have.text("hello");
+       expect(wrapper).to.include.text("hello");
    });
 
     it('should not render if completed is true', () => {
-        let wrapper = shallow(<Todo completed="{true}" text="hi"/>);
-        expect(wrapper).to.not.have.text('hi');
+        let wrapper = shallow(<Todo completed={true} text="hi"/>);
+        expect(wrapper).to.have.descendants('del');
+        expect(wrapper.find('del')).to.include.text('hi');
+    });
+
+    it('should render toggle completed butto', () => {
+       let wrapper = shallow(<Todo completed={false} text="hi"/>);
+       expect(wrapper).to.have.descendants('button');
+        expect(wrapper).to.include.text("✔");
     });
 });
