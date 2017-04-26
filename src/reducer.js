90ee0e6c198ee7d91961efcf77895a19227f5435
pushpa446 <pdumpala@thoughtworks.com>
2017-04-26 14:38:13 +0530

    add toggle completed action to reducer

--- src/reducer.js
+++ src/reducer.js
@@ -7,6 +7,10 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type){
+        case 'TOGGLE_COMPLETED':
+            const newState = JSON.parse(JSON.stringify(state));
+            newState.todos[action.index].completed = !newState.todos[action.index].completed;
+            return newState;
         default:
             return state;
     }
--- test/reducer.test.js
+++ test/reducer.test.js
@@ -2,14 +2,30 @@ import reducer from '../src/reducer';
 import {expect} from 'chai';
 
 describe('Reducer', () => {
-    it('should return 2 todos when state is undefined', () => {
-       const result = reducer(undefined, {});
-       const expectedResult = {
-           todos: [
-               {text: 'Todo 1', completed: false},
-               {text: 'Todo 2', completed: false}
-           ]
-       };
-       expect(result).to.deep.equal(expectedResult);
+
+    describe('INIT', () => {
+        it('should return 2 todos when state is undefined', () => {
+            const result = reducer(undefined, {});
+            const expectedResult = {
+                todos: [
+                    {text: 'Todo 1', completed: false},
+                    {text: 'Todo 2', completed: false}
+                ]
+            };
+            expect(result).to.deep.equal(expectedResult);
+        });
     });
+
+    describe('TOGGLE_COMPLETED', () => {
+        it('should toggle completed of the todo at ', () => {
+           const oldState = {
+               todos: [
+                   {text: 'Todo 1', completed: false}
+               ]
+           };
+           const result = reducer(oldState,{type: 'TOGGLE_COMPLETED', index: 0});
+           expect(result.todos[0].completed).to.be.true;
+        });
+    });
+
 });
